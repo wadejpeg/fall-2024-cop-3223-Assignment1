415d7eacd08c14d7b67d08849879b4426b29283d
wadejpeg <ch463276@UCF.EDU>
2024-09-08 19:36:53 -0400

    test of assignment

---
+++ assignemdt1.c
@@ -0,0 +1,32 @@
+//********************************************************
+// fracturing.c
+// Author: Charles Crawford
+// Class: COP 3223, Professor Parra
+// Purpose: write a program that will house many internal
+// functions.
+// Input: None
+//
+// Output: distance, preimeter, area, width, height.
+// //********************************************************
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+int main(int argc, char **argv) {
+
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
+
+    return 0;
+}
+
+char fkit() {
+
+    return "im tired of this, mane.";
+}
+
+//end of line
